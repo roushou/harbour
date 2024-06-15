609dad9d507d1cf4c65b39a135dd0f061bfae22d
roushou <roushou9@gmail.com>
2024-06-15 21:11:06 +0700

    feat: add simple unit test

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -7,3 +7,30 @@ pub(crate) fn router() -> Router {
 
     Router::new().route("/_health", get(handler))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn health() {
+        let api = router();
+
+        let req = Request::builder()
+            .uri("/_health")
+            .body(Body::empty())
+            .unwrap();
+
+        let response = api.oneshot(req).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"healthy");
+    }
+}
